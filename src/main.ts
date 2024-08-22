6267423c2b6b2dfb77711ca28c5fbd3daf3c34f6
Kili S <79143155+klinshy@users.noreply.github.com>
2024-08-22 15:59:13 +0200

    floating?

--- src/main.ts
+++ src/main.ts
@@ -33,4 +33,38 @@ function closePopup(){
     }
 }
 
+WA.onInit().then(() => {
+    console.log('Scripting API ready');
+    console.log('Player tags: ',WA.player.tags);
+
+
+    WA.room.area.onEnter('stairBottomToMiddle').subscribe(() => {
+        WA.nav.goToRoom("#entrymiddlefrombottom")
+    });
+
+    // From middle to bottom
+    WA.room.area.onEnter('stairMiddleToBottom').subscribe(() => {
+        WA.nav.goToRoom("#entrybottom")
+    });
+
+    // From middle to top
+    WA.room.area.onEnter('stairMiddleToTopLeft').subscribe(() => {
+        WA.nav.goToRoom("#entrytopleft")
+    });
+    WA.room.area.onEnter('stairMiddleToTopRight').subscribe(() => {
+        WA.nav.goToRoom("#entrytopright")
+    });
+
+    // From top to middle
+    WA.room.area.onEnter('stairTopToMiddleLeft').subscribe(() => {
+        WA.nav.goToRoom("#entrymiddleleft")
+    });
+    WA.room.area.onEnter('stairTopToMiddleRight').subscribe(() => {
+        WA.nav.goToRoom("#entrymiddleright")
+    });
+}).catch(e => console.error(e));
+
+
+export {};
+
 export {};
